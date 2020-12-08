ad1f11f35c24af6c3cc4718c4a5bfe6bf7681dbf
oosams <osams@softserveinc.com>
2020-12-08 23:57:33 +0200

    Update sp_StartOperation.sql

--- scripts/SP/sp_StartOperation.sql
+++ scripts/SP/sp_StartOperation.sql
@@ -27,16 +27,17 @@ BEGIN
 		FROM Logs.Operations
 		WHERE OperationID = @OperationID;
 
-
+print 1
 		-- for logging
 		DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
 			CHAR(9), '@OperationID = ', @OperationID, CHAR(13), CHAR(10),
 			CHAR(9), '@Description = ', @Description, CHAR(13), CHAR(10));
-		DECLARE @Message INT = CONCAT('Operation ''', @OperationName, ''' has been started with Parameters: ', @OperationRunParameters);
-
+		DECLARE @eventMessage INT = CONCAT('Operation ''', @OperationName, ''' has been started with Parameters: ', @OperationRunParameters);
+		DECLARE @errorMessage INT = CONCAT('Cant log the start of operation ''', @OperationName, '''  with Parameters: ', @OperationRunParameters);
+print 2
 		-- to keep new OperationRunID 
 		DECLARE @curentRunID INT;		
-
+print 3
 		INSERT INTO Logs.OperationRuns(
 			StatusID,
 			OperationID,
@@ -47,23 +48,28 @@ BEGIN
 			@OperationID,
 			CURRENT_TIMESTAMP,
 			CONCAT(@Description, ' ', @OperationDescription));
-
+print 4
+			 
 		SET @curentRunID = SCOPE_IDENTITY();
-
+print @curentRunID
 		-- throw event
 		EXEC logs.sp_SetEvent	 @runID = @curentRunID		-- INT						
 								,@affectedRows = @@rowcount		-- INT, NULL
 								,@procedureID = @@PROCID		-- INT, NULL
 								,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
-								,@eventMessage = @Message		-- NVARCHAR(MAX)
-
+								,@eventMessage = @eventMessage		-- NVARCHAR(MAX)
+print 5
 		RETURN @curentRunID;
 
 	END TRY
 	BEGIN CATCH
-
-		--TODO EXEC  sp_SetError
-
+print 6
+		--not needed?
+		EXEC logs.sp_SetError	 @runID = @curentRunID		-- INT
+								,@procedureID = @@PROCID		-- INT, NULL
+								,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+								,@errorMessage = @errorMessage	-- NVARCHAR(MAX), NULL
+	
 	END CATCH
 END
 GO
@@ -73,11 +79,11 @@ GO
 
 
 --------DEBUG---------
-DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
-		CHAR(9), '@par1 = ', 'par1', CHAR(13), CHAR(10),
-		CHAR(9), '@par1 = ', 'par1', CHAR(13), CHAR(10)
-		)
+
 
 EXEC logs.sp_StartOperation  @OperationID = 1	-- INT
 							,@Description = 'test1Description'	-- NVARCHAR(255), NULL
 							,@OperationRunParameters = 'test1OperationRunParameters'	-- NVARCHAR(MAX), NULL
+
+
+SELECT 
