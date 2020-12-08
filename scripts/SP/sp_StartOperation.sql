707afa6928fbc84c715114ac3d25b9c2f4ea893c
oosams <osams@softserveinc.com>
2020-12-09 00:39:14 +0200

    finished startOp

--- scripts/SP/sp_StartOperation.sql
+++ scripts/SP/sp_StartOperation.sql
@@ -27,18 +27,18 @@ BEGIN
 		FROM Logs.Operations
 		WHERE OperationID = @OperationID;
 
-print 1
+
 		-- for logging
 		DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
 			CHAR(9), '@OperationID = ', @OperationID, CHAR(13), CHAR(10),
 			CHAR(9), '@Description = ', @Description, CHAR(13), CHAR(10),
 			CHAR(9), '@OperationRunParameters = ', @OperationRunParameters, CHAR(13), CHAR(10)); 
-print 1	DECLARE @eventMessage NVARCHAR(MAX) = CONCAT('Operation ''', @OperationName, ''' has been started with Parameters: ', @OperationRunParameters);
-print 1	DECLARE @errorMessage NVARCHAR(MAX) = CONCAT('Cant log the start of operation ''', @OperationName, '''  with Parameters: ', @OperationRunParameters);
-print 2
+		DECLARE @eventMessage NVARCHAR(MAX) = CONCAT('Operation ''', @OperationName, ''' has been started with Parameters: ', @OperationRunParameters);
+		DECLARE @errorMessage NVARCHAR(MAX) = CONCAT('Cant log the start of operation ''', @OperationName, '''  with Parameters: ', @OperationRunParameters);
+
 		-- to keep new OperationRunID 
 		DECLARE @curentRunID INT;		
-print 3
+
 		INSERT INTO Logs.OperationRuns(
 			StatusID,
 			OperationID,
@@ -48,45 +48,32 @@ print 3
 			1, -- R,Running in Logs.OperationStatuses
 			@OperationID,
 			CURRENT_TIMESTAMP,
-			CONCAT(@Description, ' ', @OperationDescription));
-print 4
+			CONCAT(@OperationDescription, ' ', @Description));
+
 			 
 		SET @curentRunID = SCOPE_IDENTITY();
-print @curentRunID
+
 		-- throw event
 		EXEC logs.sp_SetEvent	 @runID = @curentRunID		-- INT						
 								,@affectedRows = @@rowcount		-- INT, NULL
 								,@procedureID = @@PROCID		-- INT, NULL
 								,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
 								,@eventMessage = @eventMessage		-- NVARCHAR(MAX)
-print 5
+
 		RETURN @curentRunID;
 
 	END TRY
 	BEGIN CATCH
-print 6
-		--not needed?
-		--EXEC logs.sp_SetError	 @runID = @curentRunID		-- INT
-		--						,@procedureID = @@PROCID		-- INT, NULL
-		--						,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
-		--						,@errorMessage = @errorMessage	-- NVARCHAR(MAX), NULL
 
-		   
-			DECLARE @curentErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();  
-		DECLARE @curentErrorSeverity INT = ERROR_SEVERITY();  
-		DECLARE @curentErrorState INT = ERROR_STATE();  
-		
-		RAISERROR(
-			@curentErrorMessage,   
-			@curentErrorSeverity,   
-			@curentErrorState); 
-	
+		--not needed?
+		EXEC logs.sp_SetError	 @runID = @curentRunID		-- INT
+								,@procedureID = @@PROCID		-- INT, NULL
+								,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+								,@errorMessage = @errorMessage	-- NVARCHAR(MAX), NULL
+
 	END CATCH
 END
 GO
-
-
-
 
 
 --------DEBUG---------
@@ -101,11 +88,3 @@ select * FROM Logs.EventLogs
 select * FROM Logs.ErrorLogs
 select * FROM Logs.OperationRuns
 
-
-
-
-
-
-DECLARE @time datetime
-set @time = '2020-12-09 00:13:23.363'
-SELECT @time 
