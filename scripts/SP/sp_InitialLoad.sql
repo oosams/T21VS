25a5f41da9386aed29341d840a10ce3e0631df19
oosams <osams@softserveinc.com>
2020-12-09 18:52:26 +0200

    Update sp_InitialLoad.sql

--- scripts/SP/sp_InitialLoad.sql
+++ scripts/SP/sp_InitialLoad.sql
@@ -15,81 +15,100 @@ AS
 BEGIN
 	BEGIN TRY
 
-	-- for logging
-	DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
-		CHAR(9), '@Path = ', @Path, CHAR(13), CHAR(10),
-		CHAR(9), '@FileExt = ', @FileExt, CHAR(13), CHAR(10));
+		-- for logging
+		DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
+			CHAR(9), '@Path = ', @Path, CHAR(13), CHAR(10),
+			CHAR(9), '@FileExt = ', @FileExt, CHAR(13), CHAR(10));
 
-	-- to keep new OperationRunID 
-	DECLARE @curentRunID INT;	
+		-- to keep new OperationRunID 
+		DECLARE @curentRunID INT;	
 
-	-- Start operation and get new OperationRunID
-	EXEC @curentRunID = 
-		logs.sp_StartOperation   @OperationID = 1	-- INT     OperationID for sp_InitialLoad  from Logs.Operations
-								,@Description = NULL	-- NVARCHAR(255), NULL
-								,@OperationRunParameters = @curentParameters	-- NVARCHAR(MAX), NULL
+		-- Start Operation and get new OperationRunID
+		EXEC @curentRunID = 
+			logs.sp_StartOperation   @OperationID = 1	-- INT     OperationID for sp_InitialLoad  from Logs.Operations
+									,@Description = NULL	-- NVARCHAR(255), NULL
+									,@OperationRunParameters = @curentParameters	-- NVARCHAR(MAX), NULL
 		   
 
-	-- check path existing
-	DECLARE @vFileExists Table (FileExists INT, FileDir INT, ParentDirExists INT);
+		-- check path existence, throw error and fail operation if doesn't exist
+		DECLARE @vFileExists Table (FileExists INT, FileDir INT, ParentDirExists INT);
 
-	INSERT INTO @vFileExists 
-       EXEC xp_fileexist @Path;
+		INSERT INTO @vFileExists 
+		   EXEC xp_fileexist @Path;
 
-	IF (SELECT FileDir FROM @vFileExists) = 0 
+		IF (SELECT FileDir FROM @vFileExists) = 0 
+			BEGIN
+				-- throw Error
+				EXEC logs.sp_SetError	 @runID = @curentRunID		-- INT       -- get from sp_StartOperation
+										,@procedureID = @@PROCID	-- INT, NULL
+										,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+										,@errorMessage = 'The path does not exists. '	-- NVARCHAR(MAX), NULL
+
+				-- Fail Operation
+				EXEC logs.sp_FailOperation   @OperationRunID = 	@curentRunID	 -- INT       -- get from sp_StartOperation
+											,@OperationRunParameters = @curentParameters  -- NVARCHAR(MAX), NULL
+
+				RETURN -1
+			END
+			
+		-- to concat table name from INFORMATION_SCHEMA.TABLES
+		DECLARE @table NVARCHAR (1000);
+
+		-- CURSOR to get table names from entire db
+		-- throw error if file or table doesn't exists and fetch next one
+		DECLARE CUR CURSOR FAST_FORWARD FOR
+
+			SELECT 
+				table_schema + '.' + table_name AS tablename
+			FROM   INFORMATION_SCHEMA.TABLES
+
+		OPEN CUR
+		FETCH NEXT FROM CUR INTO @table
+		WHILE @@FETCH_STATUS = 0
 		BEGIN
-			-- throw Error
-			EXEC logs.sp_SetError	 @runID = -112 		-- INT       -- get from sp_StartOperation
-									,@procedureID = @@PROCID	-- INT, NULL
-									,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
-									,@errorMessage = 'The path does not exists. '	-- NVARCHAR(MAX), NULL
+			BEGIN TRY
 
-			-- Fail Operation
-			EXEC logs.sp_FailOperation   @OperationRunID = 	@curentRunID	 -- INT
+				-- truncate each table and insert @Path + @table +@FileExt 
+				EXECUTE('truncate table ' + @table +
+					'; BULK INSERT ' + @table +
+					' FROM '''+ @Path + @table +@FileExt+''' '+
+					' with (fieldterminator = '','',rowterminator = ''0x0a'',FIRSTROW = 2, KEEPIDENTITY)')
+
+			END TRY
+			BEGIN CATCH
+
+				DECLARE @errorMessage NVARCHAR(MAX) = CONCAT('Failed to insert from file ''', @table, '.', @FileExt,'''. Check for table or file existence');	
+		
+				-- throw Error
+				EXEC logs.sp_SetError	 @runID = @curentRunID 		-- INT       -- get from sp_StartOperation
+										,@procedureID = @@PROCID	-- INT, NULL
+										,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+										,@errorMessage = @errorMessage	-- NVARCHAR(MAX), NULL
+
+			END CATCH
+			FETCH NEXT FROM CUR INTO @table
+		END
+		CLOSE CUR
+		DEALLOCATE CUR
+
+		-- Complete Operation
+		EXEC logs.sp_CompleteOperation   @OperationRunID = 	@curentRunID	 -- INT       -- get from sp_StartOperation
 										,@OperationRunParameters = @curentParameters  -- NVARCHAR(MAX), NULL
 
-			RETURN -1
-		END
-			
-	-- to concat table name from INFORMATION_SCHEMA.TABLES
-	DECLARE @table NVARCHAR (1000);
+	END TRY
+	BEGIN CATCH
 
-	-- CURSOR to get table names from entire db
-	DECLARE CUR CURSOR FAST_FORWARD FOR
+		-- throw Error
+		EXEC logs.sp_SetError	 @runID = @curentRunID 		-- INT       -- get from sp_StartOperation
+								,@procedureID = @@PROCID	-- INT, NULL
+								,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+								,@errorMessage = NULL	-- NVARCHAR(MAX), NULL
 
-		SELECT 
-			table_schema + '.' + table_name AS tablename
-		FROM   INFORMATION_SCHEMA.TABLES
+		-- Fail Operation
+		EXEC logs.sp_FailOperation   @OperationRunID = 	@curentRunID	 -- INT       -- get from sp_StartOperation
+									,@OperationRunParameters = @curentParameters  -- NVARCHAR(MAX), NULL
 
-	OPEN CUR
-	FETCH NEXT FROM CUR INTO @table
-	WHILE @@FETCH_STATUS = 0
-	BEGIN
-		BEGIN TRY
-
-			-- truncate each table and insert @Path + @table +@FileExt 
-			EXECUTE('truncate table ' + @table +
-				'; BULK INSERT ' + @table +
-				' FROM '''+ @Path + @table +@FileExt+''' '+
-				' with (fieldterminator = '','',rowterminator = ''0x0a'',FIRSTROW = 2, KEEPIDENTITY)')
-
-		END TRY
-		BEGIN CATCH
-		
-			-- throw Error
-			EXEC logs.sp_SetError	 @runID = -112 		-- INT       -- get from sp_StartOperation
-									,@procedureID = @@PROCID	-- INT, NULL
-									,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
-									,@errorMessage = CONCAT('Failed to insert from file @table.@FileExt '	-- NVARCHAR(MAX), NULL
-
-		END CATCH
-		FETCH NEXT FROM CUR INTO @table
-	END
-	CLOSE CUR
-	DEALLOCATE CUR
-
-	-- TODO EXEC  sp_CompleteOperation
-
+	END CATCH
 END
 GO
 
@@ -113,17 +132,17 @@ SELECT * FROM Logs.Operations
 -------------DEBUG----------------------------
 
 
-EXEC dbo.InitialLoad @Path = 'D:\_Work\GitHub\T21VS\scripts\generatedData\mockaroo\20201203\', @FileExt = '.csv';
+EXEC dbo.sp_InitialLoad  @Path = 'D:\_Work\GitHub\T21VS\scripts\generatedData\mockaroo\20201203\', @FileExt = '.csv';
 
 select * FROM Logs.EventLogs
 select * FROM Logs.ErrorLogs
 select * FROM Logs.OperationRuns
+
+
+------------------------------------------
+--CURSOR itself
 ------------------------------------------
 
-
-
-
---------
 
 DECLARE @table nvarchar (1000);
  DECLARE @Path nvarchar(1000) = 'D:\_Work\GitHub\T21VS\scripts\generatedData\mockaroo\20201203\', @FileExt nvarchar(1000) = '.csv';
