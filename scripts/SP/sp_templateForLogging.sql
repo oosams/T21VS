051c42afbacf1e90aa9bd35bc8cc7f156aa916a6
oosams <osams@softserveinc.com>
2020-12-13 17:21:31 +0200

    Update sp_templateForLogging.sql

--- scripts/SP/sp_templateForLogging.sql
+++ scripts/SP/sp_templateForLogging.sql
@@ -7,43 +7,59 @@ GO
 -- template
 
 CREATE OR ALTER PROCEDURE sp_templateForLogging
+	@par1 INT,
+	@par2 NVARCHAR(255) = NULL
 
 AS
 BEGIN
-	DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
-		CHAR(9), '@par1 = ', 'par1', CHAR(13), CHAR(10),
-		CHAR(9), '@par1 = ', 'par1', CHAR(13), CHAR(10))
- 
- 
-	-- EXEC sp_StartOperation ( opID from operations,  
-	
-	-- EXEC sp_SetEvent start some checks
-	-- some checks with errors
-	IF 1 = 0 
-		BEGIN
-
- 
-		-- EXEC  sp_SetEvent
-		-- EXEC  sp_SetError
-		-- EXEC  sp_FailOperation
-		RETURN -1
-		END
-	-- EXEC sp_SetEvent start some checks completed	
-
-	-- EXEC sp_SetEvent some action started	
 	BEGIN TRY
-		-- some code
-		SELECT 1
-		-- SELECT 1/ 0
-	END TRY
-	BEGIN CATCH
-		-- EXEC sp_SetEvent
-		-- EXEC  sp_SetError
-		-- EXEC  sp_FailOperation  
-		RETURN -1
-	END CATCH
+		-- for logging
+		DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
+			CHAR(9), '@par1 = ', @par1, CHAR(13), CHAR(10),
+			CHAR(9), '@par2 = ', @par2, CHAR(13), CHAR(10));
+
+		-- to keep new OperationRunID 
+		DECLARE @curentRunID INT;	
+
+		-- Start Operation and get new OperationRunID
+		EXEC @curentRunID = 
+			logs.sp_StartOperation   @OperationID = 1	-- INT     OperationID for sp_InitialLoad  from Logs.Operations
+									,@Description = NULL	-- NVARCHAR(255), NULL
+									,@OperationRunParameters = @curentParameters	-- NVARCHAR(MAX), NULL
+		  
 	
-	-- EXEC  sp_CompleteOperation
+		-- EXEC sp_SetEvent start some checks
+		-- some checks with errors
+		IF 1 = 0 
+			BEGIN 
+				-- throw Error
+				EXEC logs.sp_SetError	 @runID = @curentRunID		-- INT       -- get from sp_StartOperation
+										,@procedureID = @@PROCID	-- INT, NULL
+										,@parameters = @curentParameters	-- NVARCHAR(MAX), NULL
+										,@errorMessage = 'Check did not succssed  '	-- NVARCHAR(MAX), NULL
+
+				-- Fail Operation
+				EXEC logs.sp_FailOperation   @OperationRunID = 	@curentRunID	 -- INT       -- get from sp_StartOperation
+											,@OperationRunParameters = @curentParameters  -- NVARCHAR(MAX), NULL
+
+				RETURN -1
+			END
+		-- EXEC sp_SetEvent start some checks completed	
+
+		-- EXEC sp_SetEvent some action started	
+		BEGIN TRY
+			-- some code
+			SELECT 1
+			-- SELECT 1/ 0
+		END TRY
+		BEGIN CATCH
+			-- EXEC sp_SetEvent
+			-- EXEC  sp_SetError
+			-- EXEC  sp_FailOperation  
+			RETURN -1
+		END CATCH
+	
+		-- EXEC  sp_CompleteOperation
 
 END
 GO
