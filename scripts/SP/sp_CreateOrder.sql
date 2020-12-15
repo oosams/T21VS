be0bd03dc54cafb7b1c4ca728ebd2cb425147f53
oosams <osams@softserveinc.com>
2020-12-15 18:58:57 +0200

    Update sp_CreateOrder.sql

--- scripts/SP/sp_CreateOrder.sql
+++ scripts/SP/sp_CreateOrder.sql
@@ -8,16 +8,22 @@ GO
 
 -------------
 CREATE OR ALTER PROCEDURE shop.sp_CreateOrder
-	@EmployeeID	,
-	@CustomerID NVARCHAR(MAX),
-	@AddressID NVARCHAR(255),
-	@RequiredDate		,
-			,
-	  
-      
+	@EmployeeID	INT,
+	@CustomerID INT,
+	@AddressID INT = NULL,
+	@RequiredDate DATETIME = NULL,
+	@OrderDetails Staging.type_OrderDetails READONLY
     
 
-	
+	   ,AddressID - opt input
+      ,CustomerID - input
+      ,EmployeeID - input
+      ,OrderStatusID new 1, update when paid and shipped
+      ,OrderDate current_timestamp
+      ,RequiredDate - opt input
+      ,ShipDate update when shipped
+
+	   OrderID
       ,ProductID
       ,UnitPrice
       ,Quantity
@@ -29,8 +35,11 @@ BEGIN
 
 		-- for logging
 		DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
-			CHAR(9), '@CategoryName = ', @CategoryName, CHAR(13), CHAR(10),
-			CHAR(9), '@Description = ', @Description, CHAR(13), CHAR(10));
+			CHAR(9), '@EmployeeID = ', @EmployeeID, CHAR(13), CHAR(10),
+			CHAR(9), '@CustomerID = ', @CustomerID, CHAR(13), CHAR(10),
+			CHAR(9), '@AddressID = ', @AddressID, CHAR(13), CHAR(10),
+			CHAR(9), '@RequiredDate = ', @RequiredDate, CHAR(13), CHAR(10),
+			CHAR(9), '@OrderDetails = ', @OrderDetails, CHAR(13), CHAR(10));
 
 		
 		-- to keep new OperationRunID 
@@ -106,32 +115,54 @@ GO
 SELECT * FROM Logs.Operations
 
 --------DEBUG---------
-DROP TABLE IF EXISTS #testID 
-CREATE TABLE #testID 
-(
-	id INT
-);
-declare @iddd int
+ 
 EXEC @iddd = shop.sp_CreateCategory  @CategoryName = 'TestCatName'	 -- NVARCHAR(255)
 									,@Description = 'Test Category discription'  -- NVARCHAR(MAX)
+ 
+ 
 
-INSERT INTO #testID (id)
-SELECT @iddd
-SELECT Top 1 id FROM #testID
-
-
-DELETE Shop.Categories
-WHERE CategoryID =  (SELECT Top 1 id FROM #testID) 
-
-DBCC CHECKIDENT ('Shop.Categories')
-DBCC CHECKIDENT ('Shop.Categories', RESEED, 10)  
-  
-SELECT SCOPE_IDENTITY()
-
-SELECT * FROM Shop.Categories
+SELECT * FROM Shop.Orders
+SELECT * FROM Shop.OrderDetails
 
 SELECT * FROM Logs.EventLogs
 SELECT * FROM Logs.ErrorLogs
 SELECT * FROM Logs.OperationRuns
 SELECT * FROM Logs.Operations
 
+------------------------------
+------------------------------
+
+-------------
+CREATE OR ALTER PROCEDURE shop.sp_CreateOrder
+	@EmployeeID	INT,
+	@CustomerID INT,
+	@AddressID INT = NULL,
+	@RequiredDate DATETIME = NULL,
+	@OrderDetails Staging.type_OrderDetails READONLY
+
+AS
+	DECLARE @curentParameters NVARCHAR(MAX) =  CONCAT(
+			CHAR(9), '@EmployeeID = ', @EmployeeID, CHAR(13), CHAR(10),
+			CHAR(9), '@CustomerID = ', @CustomerID, CHAR(13), CHAR(10),
+			CHAR(9), '@AddressID = ', @AddressID, CHAR(13), CHAR(10),
+			CHAR(9), '@RequiredDate = ', @RequiredDate, CHAR(13), CHAR(10),
+			CHAR(9), '@OrderDetails = ', @OrderDetails, CHAR(13), CHAR(10));
+
+	SELECT @curentParameters
+GO
+
+
+
+DECLARE @OrderDetails Staging.type_OrderDetails
+
+INSERT INTO @OrderDetails 
+VALUES
+(1,11,12,13,14),
+(2,21,22,23,24),
+(3,31,32,33,34)
+
+EXEC shop.sp_CreateOrder
+
+
+
+select 'tab-->' + char(9) + '<--tab'
