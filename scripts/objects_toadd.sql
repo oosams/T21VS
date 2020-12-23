e5394656f3ad2a76dfd6dd57de74fd0d90b3f1ba
Oleksii Samsonov <osams@softserveinc.com>
2020-12-23 19:42:32 +0200

    ssis mapping

--- scripts/objects_toadd.sql
+++ scripts/objects_toadd.sql
@@ -23,12 +23,14 @@ GO
 
 DROP TABLE IF EXISTS Staging.VendorDelivery
 CREATE TABLE Staging.VendorDelivery(
-	 Make_Name				NVARCHAR(MAX)
+	 File_Name				NVARCHAR(MAX)
+	,Load_Date				NVARCHAR(MAX)
+	,Make_Name				NVARCHAR(MAX)
 	,Make_Desc				NVARCHAR(MAX)
-	,Make_Guid				NVARCHAR(MAX)			
+	,Make_Guid				uniqueidentifier			
 	,Product_Model_Name		NVARCHAR(MAX)			
 	,Model_Desc				NVARCHAR(MAX)
-	,Model_Guid				NVARCHAR(MAX)
+	,Model_Guid				uniqueidentifier
 	,Model_Specifications_1	NVARCHAR(MAX)
 	,Model_Specifications_2	NVARCHAR(MAX)
 	,Quantity				INT
@@ -48,8 +50,48 @@ GO
 
 
  
+SELECT a2.make_id, a2.Product_Model_id, a1.* FROM  Staging.VendorDeliveryMapped a1
+LEFT JOIN Staging.VendorDeliveryMapped a2 ON a1.model_guid = a2.model_guid
+WHERE a1.Make_id IS NULL AND a2.File_Name IS NULL
+
+SELECT * FROM  Staging.VendorDeliveryMapped
+order by file_name, make_name
+
+SELECT * FROM  Staging.VendorDelivery 
+order by file_name, make_name
+
+SELECT * FROM Logs.EventLogs
+SELECT * FROM Logs.ErrorLogs
+SELECT * FROM Logs.OperationRuns
 
 
-
-
+ 
+DROP TABLE IF EXISTS Staging.VendorDeliveryMapped
+CREATE TABLE Staging.VendorDeliveryMapped(
+	 Make_id				INT
+	,Product_Model_id		INT
+	,File_Name				NVARCHAR(MAX)
+	,Load_Date				NVARCHAR(MAX)
+	,Make_Name				NVARCHAR(MAX)
+	,Make_Desc				NVARCHAR(MAX)
+	,Make_Guid				uniqueidentifier			
+	,Product_Model_Name		NVARCHAR(MAX)			
+	,Model_Desc				NVARCHAR(MAX)
+	,Model_Guid				uniqueidentifier
+	,Model_Specifications_1	NVARCHAR(MAX)
+	,Model_Specifications_2	NVARCHAR(MAX)
+	,Quantity				INT
+	,Unit_Price				INT
+	,Add_Field_1			NVARCHAR(MAX)
+	,Add_Field_2			NVARCHAR(MAX)
+	,Add_Field_3			NVARCHAR(MAX)
+	,Add_Field_4			NVARCHAR(MAX)
+	,Add_Field_5			NVARCHAR(MAX)
+	,Add_Field_6			NVARCHAR(MAX)
+	,Add_Field_7			NVARCHAR(MAX)
+	,Add_Field_8			NVARCHAR(MAX)
+	,Add_Field_9			NVARCHAR(MAX)
+	,Add_Field_10			NVARCHAR(MAX)
+)
+GO
 
